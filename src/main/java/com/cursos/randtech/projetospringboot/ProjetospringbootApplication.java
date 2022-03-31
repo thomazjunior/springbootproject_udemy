4b16c219a94567ad54d1c79516bca8fb79bc2b59
thomazjunior <p.thomaz.junior@gmail.com>
2022-03-31 16:56:20 +0100

    Project created

---
+++ src/main/java/com/cursos/randtech/projetospringboot/ProjetospringbootApplication.java
@@ -0,0 +1,13 @@
+package com.cursos.randtech.projetospringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetospringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetospringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cursos/randtech/projetospringboot/ProjetospringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cursos.randtech.projetospringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetospringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
