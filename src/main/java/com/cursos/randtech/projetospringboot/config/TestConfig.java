96b89a3fbc05f0040cc113f84376b9fc89b6b753
thomazjunior <p.thomaz.junior@gmail.com>
2022-04-01 15:48:23 +0100

    Order table implemented

--- src/main/java/com/cursos/randtech/projetospringboot/config/TestConfig.java
+++ src/main/java/com/cursos/randtech/projetospringboot/config/TestConfig.java
@@ -10,6 +10,10 @@ import org.springframework.context.annotation.Profile;
 import com.cursos.randtech.projetospringboot.entites.User;
 import com.cursos.randtech.projetospringboot.repositories.UserRepository;
 
+/**
+ * @author paulo.junior
+ *
+ */
 @Configuration
 @Profile("test")
 public class TestConfig implements CommandLineRunner {
@@ -21,6 +25,7 @@ public class TestConfig implements CommandLineRunner {
 	public void run(String... args) throws Exception {
 		User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
 		User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456"); 
+		User u3 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456"); 
 		
 		userRepository.saveAll(Arrays.asList(u1, u2));
 		
--- src/main/java/com/cursos/randtech/projetospringboot/entites/User.java
+++ src/main/java/com/cursos/randtech/projetospringboot/entites/User.java
@@ -1,17 +1,24 @@
 package com.cursos.randtech.projetospringboot.entites;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
 
 @Entity
+@Table(name = "tb_user")
 public class User implements Serializable {
 	
 	private static final long serialVersionUID = 1L;
+	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
@@ -19,6 +26,10 @@ public class User implements Serializable {
 	private String email; 
 	private String phone; 
 	private String password; 
+	
+	@OneToMany(mappedBy = "client")
+	private List<Order> orders = new ArrayList<>();
+	
 	
 	public User() {
 		
@@ -32,7 +43,12 @@ public class User implements Serializable {
 		this.phone = phone;
 		this.password = password;
 	}
-
+	
+	public List<Order> getOrders() {
+		return orders;
+	}
+	
+	
 	public Long getId() {
 		return id;
 	}
@@ -89,6 +105,7 @@ public class User implements Serializable {
 		User other = (User) obj;
 		return Objects.equals(id, other.id);
 	}
+
 	
 	
 	
