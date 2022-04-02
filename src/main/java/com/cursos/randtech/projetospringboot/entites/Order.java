55bf9cd7eef5ff26f4406b04dee632feed66e099
thomazjunior <p.thomaz.junior@gmail.com>
2022-04-02 14:57:07 +0100

    Fix of Order enum code

--- src/main/java/com/cursos/randtech/projetospringboot/entites/Order.java
+++ src/main/java/com/cursos/randtech/projetospringboot/entites/Order.java
@@ -12,22 +12,23 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
+import com.cursos.randtech.projetospringboot.entites.enums.OrderStatus;
 import com.fasterxml.jackson.annotation.JsonFormat;
 
 @Entity
 @Table(name = "tb_order")
 public class Order implements Serializable {
 
-	
 	private static final long serialVersionUID = 1L;
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	
+
 	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
 	private Instant moment;
-	
-	
+
+	private Integer orderStatus;
+
 	@ManyToOne
 	@JoinColumn(name = "client_id")
 	private User client;
@@ -36,10 +37,11 @@ public class Order implements Serializable {
 
 	}
 
-	public Order(Long id, Instant moment, User client) {
+	public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
 		super();
 		this.id = id;
 		this.moment = moment;
+		setOrderStatus(orderStatus);;
 		this.client = client;
 	}
 
@@ -65,6 +67,16 @@ public class Order implements Serializable {
 
 	public void setClient(User client) {
 		this.client = client;
+	}
+
+	public OrderStatus getOrderStatus() {
+		return OrderStatus.valueOf(orderStatus);
+	}
+
+	public void setOrderStatus(OrderStatus orderStatus) {
+		if (orderStatus != null) {
+			this.orderStatus = orderStatus.getCode();
+		}
 	}
 
 	@Override
--- src/main/java/com/cursos/randtech/projetospringboot/services/OrderService.java
+++ src/main/java/com/cursos/randtech/projetospringboot/services/OrderService.java
@@ -19,6 +19,8 @@ public class OrderService {
 		return repository.findAll();
 	}
 	
+	
+	
 	public Order findById (Long id) {
 		Optional<Order> obj = repository.findById(id);
 		return obj.get();
