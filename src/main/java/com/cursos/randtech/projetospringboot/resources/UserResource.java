b3852bdb8a93c4e39b7a175647c78c643ef37bc5
thomazjunior <p.thomaz.junior@gmail.com>
2022-04-04 13:01:55 +0100

    finalized

--- src/main/java/com/cursos/randtech/projetospringboot/resources/UserResource.java
+++ src/main/java/com/cursos/randtech/projetospringboot/resources/UserResource.java
@@ -42,7 +42,9 @@ public class UserResource {
 	@PostMapping
 	public ResponseEntity<User> insert(@RequestBody User obj){
 		obj = service.insert(obj);
-		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+				.path("/{id}")
+				.buildAndExpand(obj.getId()).toUri();
 		return ResponseEntity.created(uri).body(obj);
 	}
 	
